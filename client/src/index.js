79db7d4b2f2232c06bc7a4dd325e2b31201bbe00
dhillon-Prabh <psingh143105@gmail.com>
2019-09-28 18:00:01 -0700

    changed client folder structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './css/index.css';
+import App from './js/components/App';
+import * as serviceWorker from './js/components/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import axios from "axios";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../images/logo.svg';
+import '../../css/App.css';
 
 class App extends Component {
 

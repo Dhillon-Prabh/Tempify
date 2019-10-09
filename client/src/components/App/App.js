29e619ef7db5d9ffc67b074f3c8e977ce8818114
John Ham <hamj1990@gmail.com>
2019-10-09 00:23:00 -0700

    Block component, css still needs work

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from '../Navbar/Navbar'
+import {Block1, Block2, Block3, Block4} from '../Block/Block'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Navbar/>
+        <Block1/>
+        <Block2/>
+        <Block3/>
+        <Block4/>
       </div>
     )
   }

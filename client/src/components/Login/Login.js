dfaa00bc0312d6f515a812ec79d5a0e6caafe150
joeco fong <joecofg@gmail.com>
2019-10-10 16:48:35 -0700

    fixed a font size error

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -15,7 +15,6 @@ const useStyles = makeStyles(theme => ({
   '@global': {
     body: {
       backgroundColor: theme.palette.common.white,
-      fontFamily: 'Source Sans Pro'
     },
   },
   paper: {

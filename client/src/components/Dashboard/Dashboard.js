b2cd1203e6220adbd4f901d53c3612ab0429afe7
dhillon-Prabh <psingh143105@gmail.com>
2019-10-28 00:07:01 -0700

    dashboard booknow working

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Dashboard.css';
 import { Grid } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography'
+import {Link} from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import SvgIcon from '@material-ui/core/SvgIcon';
+import FindFit from '../BookNow/BookNow';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 function HomeIcon(props) {
     return (
@@ -16,21 +22,31 @@ function HomeIcon(props) {
 
 const DashboardOptions = () => {
     return(
-        <Grid container direction="row" alignItems="center" justify="center" className="options">
-            <Grid item alignContent="center">
-                <HomeIcon />
+        <React.Fragment>
+            <Grid container direction="row" justify="center" alignItems="center" className="options">
+                <Grid item className="optionsLink">
+                    <Breadcrumbs aria-label="breadcrumb">
+                        <Link to="/home" style={{textDecoration:'none', color: 'inherit'}}>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <HomeIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Home" />
+                            </ListItem>
+                        </Link>
+                        <Typography color="textPrimary">dashboard</Typography>
+                    </Breadcrumbs>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                    <ButtonGroup fullWidth aria-label="full width outlined button group" className="buttons">
+                        <Button className="button">Book Now</Button>
+                        <Button className="button">Schedule</Button>
+                        <Button className="button">History</Button>
+                    </ButtonGroup>
+                </Grid>
             </Grid>
-            <Grid item alignContent="center">
-                <Typography> Home</Typography>
-            </Grid>
-            <Grid item xs={12} md={3}>
-                <ButtonGroup fullWidth aria-label="full width outlined button group" className="buttons">
-                    <Button className="button">Book Now</Button>
-                    <Button className="button">Schedule</Button>
-                    <Button className="button">History</Button>
-                </ButtonGroup>
-            </Grid>
-        </Grid>
+            <FindFit/>
+        </React.Fragment>
     );
 }
 

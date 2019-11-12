0d006f2179cd1ac29c35456bb7b48b60ab8fdceb
dhillon-Prabh <psingh143105@gmail.com>
2019-11-12 00:26:19 -0800

    promise implemented

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -56,8 +56,7 @@ exports.tempRegister = (req, res, next) => {
       return new Promise(function (resolve, reject) {
         var userQuery = 'INSERT INTO users(name, email, password, remember_token, created_at, updated_at,' +
         'server_response, role, current_login_time,' +
-        'last_login_time, status, unsubscribe_from_emails, unsubscribe_modules) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);' +
-        'SELECT LAST_INSERT_ID();';
+        'last_login_time, status, unsubscribe_from_emails, unsubscribe_modules) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);';
         values=[user.name, user.email, user.password, null, new Date(), new Date(), null, 2, null, null, 1, 0, null];
         con.query(userQuery, values, (err, result, fields) => {
           if(!err) {
@@ -66,14 +65,28 @@ exports.tempRegister = (req, res, next) => {
           } else {
             reject(err);
           }
-        })
-      })
+        });
+      });
     }
     addUser().then(function(result) {
-      console.log("this witll and shall og");
+      var tempQuery = 'INSERT INTO temps(`type_of_practice`, `imagename`, `email`, `expected_rate`, `license_number`,' +
+      '`temp_name`, `designation`, `is_assistant`, `is_hygienist`, `is_receptionist`, `experience`, `is_approved`,' +
+      ' `dental_software`, `city`, `user_id`, `updated_at`, `created_at`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);';
+      valuesTemp=[user.practice, null, user.email, user.expectedRate, user.license, user.name, user.role, 1, 1, 1, user.experience, 0, user.dentalsw,
+          user.city, result.insertId, new Date(), new Date()];
+        con.query(tempQuery, valuesTemp, (err, result, fields) => {
+          if(!err) {
+            console.log("no error proceeding to success");
+            res.status(300).send({ message: "success" });
+            con.release();
+          } else {
+            reject(err);
+          }
+        })
     });
     addUser().catch(function(err) {
       console.log("Error:" + err);
+      res.status(401).send({error: "unable to complete request"});
       con.release();
     })
   }

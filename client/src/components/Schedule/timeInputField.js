665f5ba7117b5e204eda9fb1a504cf9c7e990138
dhillon-Prabh <psingh143105@gmail.com>
2019-11-26 19:52:19 -0800

    adding hours'

--- client/src/components/Schedule/timeInputField.js
+++ client/src/components/Schedule/timeInputField.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from "./paymentRequestButton";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = theme => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
@@ -15,16 +15,32 @@ const useStyles = makeStyles(theme => ({
       marginLeft: "25px"
     },
   },
-}));
+});
 
-export default class BasicTextFields extends React.Fragment {
-  const classes = useStyles();
+class BasicTextFields extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hour: '',
+      minutes: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  render() { 
+    const { classes } = this.props;
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="standard-basic" label="Hour" />
-      <TextField id="standard-basic" label="Minute" />
-      <Button />
-    </form>
-  );
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="standard-basic" name="hour" value={this.state.hour} onChange={this.handleChange} label="Hour" />
+        <TextField id="standard-basic" name="minutes" value={this.state.minutes} onChange={this.handleChange} label="Minute" />
+        <Button />
+      </form>
+    );
+  }
 }
+
+export default withStyles(useStyles)(BasicTextFields);

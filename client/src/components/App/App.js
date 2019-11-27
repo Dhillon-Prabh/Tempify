e6f069ba099157ef09e9579172d3277984d33776
Oscar <au.oscar@hotmail.com>
2019-11-27 10:28:12 -0800

    pls

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -4,13 +4,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
 import Navbar from "../Navbar/Navbar";
 import Cookie from "../Cookie/Cookie";
-import Schedule from "../Schedule/scheduleForTemp";
-import ContactSection from "../Contact/ContactSection";
-import OfficeProfileCard from "../ProfileCard/OfficeProfileCard";
-import OfficeSchedule from "../Schedule/scheduleForOffice";
-import TempSchedule from "../Schedule/scheduleForTemp"
-import Button from "../Payment/PayButton"
-import Modal from "../Schedule/OfficeModal"
+import DashBoard from "../Dashboard/Dashboard"
 
 class App extends Component {
   render() {

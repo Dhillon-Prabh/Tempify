41e00b80cfe6283106e89f09bff7f23f0c70dd55
dhillon-Prabh <psingh143105@gmail.com>
2019-11-30 19:56:24 -0800

    minor addition

--- client/src/components/Profile/DentalProfile.js
+++ client/src/components/Profile/DentalProfile.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';

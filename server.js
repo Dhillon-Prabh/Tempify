6861a744140d917bbcc5a1955152ed6293e93bbf
joeco fong <joecofg@gmail.com>
2019-12-01 12:18:02 -0800

    make it better

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use('/auth', routes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, './client/build')));
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendfile(path.join(__dirname = './client/build/index.html'));
   });
 }

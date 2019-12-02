c02168c705fe02a3ae0d9e0a2eeb72efc5fed0d9
joeco fong <joecofg@gmail.com>
2019-12-02 00:53:22 -0800

    make it better

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.use(bodyParser.json());
 //                    Gets Around CORS ISSUE                                    // 
 //            Will be unneccesary once we serve the build file                  // 
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   res.setHeader('preflightContinue', 'false');
-//   next();
-// })
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('preflightContinue', 'false');
+  next();
+})
 
 app.use('/', routes);
 

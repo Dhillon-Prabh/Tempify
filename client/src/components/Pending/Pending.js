5bbf9d57907bdb342892bc2f6e7404c016ed9f8d
joeco fong <joecofg@gmail.com>
2019-12-02 07:02:32 -0800

    added comments to more places

--- client/src/components/Pending/Pending.js
+++ client/src/components/Pending/Pending.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 import './Pending.css'
 
+/**
+ * Shows 'Pending' gigs. Client mentioned in previous meetings 
+ * that this feature may be removed so as of now it is not complete. 
+ * @author Joe Fong 
+ * @version 1.0 
+ */
 class Pending extends Component {
 
   render(){
